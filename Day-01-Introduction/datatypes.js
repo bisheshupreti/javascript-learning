c53eb21827115204d25b0b8ae90fb74ac5468433
Bishesh Upreti <bisheshupreti74@gmail.com>
2026-07-07 17:48:48 +0545

    Day 1 - Datatypes and typeof operator

---
+++ Day-01-Introduction/datatypes.js
@@ -0,0 +1,18 @@
+let name = "Spe";        // String
+let age = 21;            // Number
+let isStudent = true;    // Boolean
+let address;             // Undefined
+let marks = null;        // Null
+let salary = 3000.45;
+
+let student = { name: "Spe", age: 21 }; // Object
+let colors = ["red", "blue"];           // Array
+
+console.log(typeof name);   // "string"
+console.log(typeof age);        // "number"
+console.log(typeof isStudent);   // "boolean"
+console.log(typeof address); // "undefined"
+console.log(typeof marks);      // "object"  (this is a famous long-standing JS quirk!)
+console.log(typeof salary); 
+console.log(typeof student); 
+console.log(typeof colors); 
