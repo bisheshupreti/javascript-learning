f8e4e3b4cfde75388cc61187f43cff21264ce495
Bishesh Upreti <bisheshupreti74@gmail.com>
2026-07-08 12:53:04 +0545

    Day 2 - Control Structure : if-else

---
+++ Day-02-Control-Structure,-Arrays-and-Functions/1.ifelse.js
@@ -0,0 +1,28 @@
+let age = 20;
+//IF
+if (age >= 18) {
+  console.log("You are an adult.");
+}
+
+
+age = 15;
+// if..........else
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
+
+
+// if...else if...else
+let marks = 75;
+
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 75) {
+  console.log("Grade B");
+} else if (marks >= 50) {
+  console.log("Grade C");
+} else {
+  console.log("Fail");
+}
