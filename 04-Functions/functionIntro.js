9809e0729ea8b31517baeb84cb75403dc3cf3a02
Bishesh Upreti <bisheshupreti74@gmail.com>
2026-07-29 20:14:02 +0545

    Functions Introduction is Done

---
+++ 04-Functions/functionIntro.js
@@ -0,0 +1,16 @@
+//Defining Function
+function greet() {
+  console.log("Welcome to JavaScript!");
+}
+
+//Invoking function
+greet();
+
+
+//Function With Parameter
+function sq(a) {
+  return a * a;
+}
+
+let square = sq(5);
+console.log(square);
